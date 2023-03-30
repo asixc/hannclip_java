06dc3db5d4cb560416d57eb66c96a41b3f700574
Jose Alberto <jose.albertolorenzo.sge@mango.com>
2023-03-30 23:42:55 +0200

    spring init

---
+++ src/main/java/dev/jotxee/hannclip/HannclipApplication.java
@@ -0,0 +1,13 @@
+package dev.jotxee.hannclip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HannclipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HannclipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jotxee/hannclip/HannclipApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jotxee.hannclip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HannclipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
